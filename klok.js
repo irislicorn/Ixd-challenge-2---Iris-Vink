e0401cb4c85e727ca1f0d44668adbd27a0cfc38d
Irislicorn <irislicorn@github.com>
2018-03-20 19:28:17 +0100

    s
    dd

--- klok.js
+++ klok.js
@@ -1,10 +1,18 @@
-function time ();{
+function time (){
 
 var d = new Date();
 document.getElementById("demo").innerHTML = d.toString();}
 
 
+test();
+function test() {
+	var tijd = 3600;
+
+	TweenLite.to(".front", tijd, {
+		rotationY: -5.4*tijd,
+		ease: Linear.easeNone,
+	});
+	
+}
 
 
-window.onload = function(){
- TweenLite.to("#logo", 10, {left:600}); }
